70b0502ae620dcd9f588fbaf0fcff01d83b75198
FomenkoArtur <artur-fomenko@list.ru>
2024-12-06 17:37:48 +0200

    find-max.py:changed

--- find-max.py
+++ find-max.py
@@ -1,4 +1,16 @@
 def find_max(num):
-    return max(num)
-nums = [1, 2, 3, 4, 5]
+    if not num:
+        print ("Список не должен быть пустым")
+    max = num[0]
+    for n in num:
+        if n > max:
+            max = n
+    return max
+n = int(input("Введите количество чисел: "))
+if n <= 0:
+        print ("Количество чисел должно быть положительным")
+nums = []
+for i in range(n):
+        number = float(input(f"Введите число {i + 1}: "))
+        nums.append(number)
 print("Максимум:", find_max(nums))
